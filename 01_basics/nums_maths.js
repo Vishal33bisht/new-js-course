2c3570e10147e58144fb28dc2c390380222e924a
Vishal bisht <145672156+Vishal33bisht@users.noreply.github.com>
2025-08-26 15:26:33 +0000

    math and num

---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,11 @@
+const balance=new Number(100)
+//console.log(balance);
+//console.log(balance.toString())
+const number=123.875
+//console.log(number.toPrecision(4))
+const hundred=100000
+//console.log(hundred.toLocaleString('en-In'))
+console.log(Math)
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random() * (max-min))+min)
